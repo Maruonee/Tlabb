609bc5f62a38d3c90ce8609905f1fa6fe72d31c6
Maruonee <vole91@gmail.com>
2022-09-25 18:43:11 +0900

    v6

--- editdata/split.py
+++ editdata/split.py
@@ -12,7 +12,7 @@ input/
         ...
     ...
 """
-input_folder = '/home/tlab/Dataset/image'
+input_folder = '/home/tlab/dataset/img/'
 #아웃풋 폴더
 """
 output/
@@ -38,7 +38,7 @@ output/
             imgc.jpg
             ...           
 """
-out_foloder = '/home/tlab/dataset/output'
+out_foloder = '/home/tlab/dataset/img/output/'
 #학습비율
 train_ratio = 0.6
 #검증비율
