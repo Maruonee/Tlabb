d5162aee2d719d6b04e14b42e1d440ab779a7d75
Maruonee <vole91@gmail.com>
2023-07-31 11:29:36 +0900

    dd

--- classfication/TensorClass/Classification.py
+++ classfication/TensorClass/Classification.py
@@ -9,6 +9,7 @@ from keras.models import load_model
 import time
 from tensorflow.compat.v1 import ConfigProto
 from tensorflow.compat.v1 import InteractiveSession
+
 #GPU오류 수정
 def fix_gpu():
     config = ConfigProto()
@@ -16,6 +17,7 @@ def fix_gpu():
     session = InteractiveSession(config=config)
     print(session)
 fix_gpu()
+
 #Recall Precision F1score
 from keras import backend as K
 def recall_m(y_true, y_pred):
