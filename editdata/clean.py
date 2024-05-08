93d2b55fd0eadc3bde6a2630e192cb126cd78519
Maruonee <vole91@gmail.com>
2024-05-08 12:51:08 +0900

    asd

--- editdata/clean.py
+++ editdata/clean.py
@@ -3,7 +3,7 @@ import glob
 
 def delete_contents_in_files(directory):
     # 주어진 디렉토리에서 'not fallen'으로 시작하는 모든 .txt 파일 검색
-    pattern = os.path.join(directory, 'not fallen*.txt')
+    pattern = os.path.join(directory, 'v_not fallen*.txt')
     files = glob.glob(pattern)
     
     # 각 파일을 열어 내용을 삭제
