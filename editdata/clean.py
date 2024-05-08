872dc6ebc3d7a8225a7d10731d254fe67a9ade00
Maruonee <vole91@gmail.com>
2024-05-08 12:51:53 +0900

    123

--- editdata/clean.py
+++ editdata/clean.py
@@ -3,7 +3,7 @@ import glob
 
 def delete_contents_in_files(directory):
     # 주어진 디렉토리에서 'not fallen'으로 시작하는 모든 .txt 파일 검색
-    pattern = os.path.join(directory, 'not fallen*.txt')
+    pattern = os.path.join(directory, 'v_not fallen*.txt')
     files = glob.glob(pattern)
     
     # 각 파일을 열어 내용을 삭제
@@ -12,5 +12,5 @@ def delete_contents_in_files(directory):
             file.truncate()
 
 # 사용 예:
-directory_path = '/home/tlab4090/datasets/labels'
+directory_path = '/home/tlab1004/datasets/labels'
 delete_contents_in_files(directory_path)
