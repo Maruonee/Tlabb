b0f92b5af11d3428bb15472b9ad7d71f857ea607
Maruonee <vole91@gmail.com>
2024-07-02 00:05:23 +0900

    ddgdas

--- OLTC/OLTC_Tlab_python/utils/folder_helper.py
+++
@@ -1,7 +0,0 @@
-import os  # 운영 체제 모듈 가져오기
-
-def create_folder(savedir, exp_date, exp_num, suffix):
-    folder_name = f"{exp_date}_{exp_num}_{suffix}"  # 폴더 이름 생성
-    recordings_folder_path = os.path.join(savedir, str(exp_date), str(exp_num), folder_name)  # 폴더 경로 생성
-    os.makedirs(recordings_folder_path, exist_ok=True)  # 폴더 생성 (존재하지 않는 경우)
-    return recordings_folder_path  # 폴더 경로 반환
